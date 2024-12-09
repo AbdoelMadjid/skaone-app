6b2426cfd2a8ef698db59fa27fc4167e3b39fcd8
abdoelmadjid <abdulmadjid.mpd@gmail.com>
2024-12-09 14:04:26 +0700

    update dashboard

--- resources/views/dashboard/dashboard-personil.blade.php
+++ resources/views/dashboard/dashboard-personil.blade.php
@@ -210,39 +210,42 @@
             <div class="card-body">
                 <div class="ribbon ribbon-primary round-shape">Peserta Didik Login Hari ini</div>
                 <div class="ribbon-content mt-5 text-muted">
-                    @if ($userLoginHariiniSiswa->isEmpty())
-                        <p>No users have logged in today.</p>
-                    @else
-                        <div class="row">
-                            @foreach ($userLoginHariiniSiswa->chunk(2) as $userRow)
-                                <div class="row">
-                                    @foreach ($userRow as $user)
-                                        <div class="col-md-6">
-                                            <div
-                                                class="d-flex justify-content-between border-bottom border-bottom-dashed py-0">
-                                                <p class="fw-medium mb-0">
-                                                    @if ($user->avatar == 'siswacewek.png' || $user->avatar == 'siswacowok.png')
-                                                        <img src="{{ URL::asset('build/images/users/user-dummy-img.jpg') }}"
-                                                            alt="" class="avatar-xs rounded-circle me-2">
-                                                    @else
-                                                        <img src="{{ URL::asset('images/thumbnail/' . $user->avatar . '') }}"
-                                                            alt="" class="avatar-xs rounded-circle me-2">
-                                                    @endif
-                                                    {{ $user->nis }} -
-                                                    {!! $user->name !!}
-                                                </p>
-                                                <div>
-                                                    <span
-                                                        class="text-success fw-medium fs-12">{{ $user->login_count }}
-                                                        x</span>
-                                                </div>
-                                            </div><!-- end -->
-                                        </div><!-- end col -->
-                                    @endforeach
-                                </div><!-- end row -->
-                            @endforeach
-                        </div>
-                    @endif
+                    <div data-simplebar data-simplebar-auto-hide="false" data-simplebar-track="secondary"
+                        style="max-height: 374px;">
+                        @if ($userLoginHariiniSiswa->isEmpty())
+                            <p>No users have logged in today.</p>
+                        @else
+                            <div class="row">
+                                @foreach ($userLoginHariiniSiswa->chunk(2) as $userRow)
+                                    <div class="row">
+                                        @foreach ($userRow as $user)
+                                            <div class="col-md-6">
+                                                <div
+                                                    class="d-flex justify-content-between border-bottom border-bottom-dashed py-0">
+                                                    <p class="fw-medium mb-0">
+                                                        @if ($user->avatar == 'siswacewek.png' || $user->avatar == 'siswacowok.png')
+                                                            <img src="{{ URL::asset('build/images/users/user-dummy-img.jpg') }}"
+                                                                alt="" class="avatar-xs rounded-circle me-2">
+                                                        @else
+                                                            <img src="{{ URL::asset('images/thumbnail/' . $user->avatar . '') }}"
+                                                                alt="" class="avatar-xs rounded-circle me-2">
+                                                        @endif
+                                                        {{ $user->nis }} -
+                                                        {!! $user->name !!}
+                                                    </p>
+                                                    <div>
+                                                        <span
+                                                            class="text-success fw-medium fs-12">{{ $user->login_count }}
+                                                            x</span>
+                                                    </div>
+                                                </div><!-- end -->
+                                            </div><!-- end col -->
+                                        @endforeach
+                                    </div><!-- end row -->
+                                @endforeach
+                            </div>
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
