6a46b9d7d74d7d321775b3c4693185e2d15c5a60
AbdoelMadjid <abdulmadjid.mpd@gmail.com>
2024-12-10 12:05:15 +0700

    update guru mapel

--- app/Http/Controllers/GuruMapel/DeskripsiNilaiController.php
+++ app/Http/Controllers/GuruMapel/DeskripsiNilaiController.php
@@ -140,7 +140,7 @@ class DeskripsiNilaiController extends Controller
 
         // Jalankan query untuk mendapatkan data siswa beserta nilai formatif dan sumatif
         $data = DB::select("
-        SELECT
+            SELECT
             personil_sekolahs.gelardepan,
             personil_sekolahs.namalengkap,
             personil_sekolahs.gelarbelakang,
@@ -151,26 +151,39 @@ class DeskripsiNilaiController extends Controller
             kbm_per_rombels.mata_pelajaran,
             peserta_didik_rombels.nis,
             peserta_didiks.nama_lengkap,
-            nilai_formatif.*,
+            nilai_formatif.tp_isi_1,
+            nilai_formatif.tp_isi_2,
+            nilai_formatif.tp_isi_3,
+            nilai_formatif.tp_isi_4,
+            nilai_formatif.tp_isi_5,
+            nilai_formatif.tp_isi_6,
+            nilai_formatif.tp_isi_7,
+            nilai_formatif.tp_isi_8,
+            nilai_formatif.tp_isi_9,
+            nilai_formatif.tp_nilai_1,
+            nilai_formatif.tp_nilai_2,
+            nilai_formatif.tp_nilai_3,
+            nilai_formatif.tp_nilai_4,
+            nilai_formatif.tp_nilai_5,
+            nilai_formatif.tp_nilai_6,
+            nilai_formatif.tp_nilai_7,
+            nilai_formatif.tp_nilai_8,
+            nilai_formatif.tp_nilai_9,
+            nilai_formatif.rerata_formatif,
             nilai_sumatif.sts,
             nilai_sumatif.sas,
             nilai_sumatif.kel_mapel AS kel_mapel_sumatif,
             nilai_sumatif.rerata_sumatif,
             ((COALESCE(nilai_formatif.rerata_formatif, 0) + COALESCE(nilai_sumatif.rerata_sumatif, 0)) / 2) AS nilai_na
         FROM kbm_per_rombels
-        INNER JOIN peserta_didik_rombels
-            ON kbm_per_rombels.kode_rombel = peserta_didik_rombels.rombel_kode
-        INNER JOIN peserta_didiks
-            ON peserta_didik_rombels.nis = peserta_didiks.nis
-            INNER JOIN personil_sekolahs
-                ON kbm_per_rombels.id_personil = personil_sekolahs.id_personil
-        LEFT JOIN nilai_formatif
-            ON peserta_didik_rombels.nis = nilai_formatif.nis
+        INNER JOIN peserta_didik_rombels ON kbm_per_rombels.kode_rombel = peserta_didik_rombels.rombel_kode
+        INNER JOIN peserta_didiks ON peserta_didik_rombels.nis = peserta_didiks.nis
+        INNER JOIN personil_sekolahs ON kbm_per_rombels.id_personil=personil_sekolahs.id_personil
+        LEFT JOIN nilai_formatif ON peserta_didik_rombels.nis = nilai_formatif.nis
             AND nilai_formatif.id_personil = ?
             AND nilai_formatif.kode_rombel = ?
             AND nilai_formatif.kel_mapel = ?
-        LEFT JOIN nilai_sumatif
-            ON peserta_didik_rombels.nis = nilai_sumatif.nis
+        LEFT JOIN nilai_sumatif ON peserta_didik_rombels.nis = nilai_sumatif.nis
             AND nilai_sumatif.id_personil = ?
             AND nilai_sumatif.kode_rombel = ?
             AND nilai_sumatif.kel_mapel = ?
@@ -179,7 +192,7 @@ class DeskripsiNilaiController extends Controller
             AND kbm_per_rombels.kode_rombel = ?
             AND kbm_per_rombels.kel_mapel = ?
         ORDER BY peserta_didik_rombels.nis
-    ", [$id_personil, $kode_rombel, $kel_mapel, $id_personil, $kode_rombel, $kel_mapel, $id_personil, $kode_rombel, $kel_mapel]);
+        ", [$id_personil, $kode_rombel, $kel_mapel, $id_personil, $kode_rombel, $kel_mapel, $id_personil, $kode_rombel, $kel_mapel]);
 
         return response()->json([
             'data' => $data,
