a68deb27d048900f57d168ca72724be47e499e26
abdoelmadjid <abdulmadjid.mpd@gmail.com>
2024-12-12 20:07:36 +0700

    update cetak rapor

--- resources/views/pages/kurikulum/dokumensiswa/cetak-rapor-pilihdata.blade.php
+++ resources/views/pages/kurikulum/dokumensiswa/cetak-rapor-pilihdata.blade.php
@@ -5,69 +5,72 @@
             <div class="card-body">
                 <div class="ribbon ribbon-success round-shape">Pilih Data Cetak</div>
                 <h5 class="fs-14 text-end">{{ $personal_id }}</h5>
-                <div class="ribbon-content mt-5">
-                    <div class="row">
-                        <div class="col-md-3">
-                            <label for="tahunajaran">Tahun Ajaran</label>
-                            <select class="form-control mb-3" name="tahunajaran" id="tahunajaran" required>
-                                <option value="" selected>Pilih TA</option>
-                                @foreach ($tahunAjaranOptions as $tahunajaran => $thajar)
-                                    <option value="{{ $tahunajaran }}">{{ $thajar }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-md-3">
-                            <label for="tingkat">Semester</label>
-                            <select class="form-control mb-3" name="semester" id="semester" required>
-                                <option value="" selected>Pilih Semester</option>
-                                <option value="Ganjil">Ganjil</option>
-                                <option value="Genap">Genap</option>
-                            </select>
-                        </div>
-                        <div class="col-md-4">
-                            <label for="kode_kk">Kompetensi Keahlian</label>
-                            <select class="form-control mb-3" name="kode_kk" id="kode_kk" required>
-                                <option value="" selected>Pilih Kompetensi Keahlian</option>
-                                @foreach ($kompetensiKeahlianOptions as $idkk => $nama_kk)
-                                    <option value="{{ $idkk }}">{{ $nama_kk }}</option>
-                                @endforeach
-                            </select>
-                        </div>
+                <form action="{{ route('kurikulum.dokumentsiswa.cetak-rapor.store') }}" method="post">
+                    @csrf
+                    <div class="ribbon-content mt-5">
+                        <div class="row">
+                            <div class="col-md-3">
+                                <label for="tahunajaran">Tahun Ajaran</label>
+                                <select class="form-control mb-3" name="tahunajaran" id="tahunajaran" required>
+                                    <option value="" selected>Pilih TA</option>
+                                    @foreach ($tahunAjaranOptions as $tahunajaran => $thajar)
+                                        <option value="{{ $tahunajaran }}">{{ $thajar }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-md-3">
+                                <label for="tingkat">Semester</label>
+                                <select class="form-control mb-3" name="semester" id="semester" required>
+                                    <option value="" selected>Pilih Semester</option>
+                                    <option value="Ganjil">Ganjil</option>
+                                    <option value="Genap">Genap</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4">
+                                <label for="kode_kk">Kompetensi Keahlian</label>
+                                <select class="form-control mb-3" name="kode_kk" id="kode_kk" required>
+                                    <option value="" selected>Pilih Kompetensi Keahlian</option>
+                                    @foreach ($kompetensiKeahlianOptions as $idkk => $nama_kk)
+                                        <option value="{{ $idkk }}">{{ $nama_kk }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
 
-                        <div class="col-md-2">
-                            <label for="tingkat">Tingkat</label>
-                            <select class="form-control mb-3" name="tingkat" id="tingkat" required>
-                                <option value="" selected>Pilih Tingkat</option>
-                                <option value="10">10</option>
-                                <option value="11">11</option>
-                                <option value="12">12</option>
-                            </select>
-                        </div>
-                        <div class="col-md-4">
-                            <label for="kode_kk">Rombongan Belajar</label>
-                            <select class="form-control mb-3" name="kode_rombel" id="kode_rombel" required>
-                                <option value="" selected>Pilih Rombel</option>
-                                @foreach ($rombelOptions as $kode_rombel => $rombel)
-                                    <option value="{{ $kode_rombel }}">{{ $rombel }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="col-md-4">
-                            <label for="kode_peserta_didik">Peserta Didik</label>
-                            <select class="form-control mb-3" name="kode_peserta_didik" id="kode_peserta_didik"
-                                required>
-                                <option value="" selected>Pilih Peserta Didik</option>
-                                <!-- Data akan dimuat secara dinamis -->
-                            </select>
-                        </div>
-                        <div class="col-lg-12">
-                            <div class="gap-2 hstack justify-content-end">
-                                <button type="submit" class="btn btn-soft-success">Simpan</button>
-                                {{-- <button type="button" class="btn btn-soft-success">Cancel</button> --}}
+                            <div class="col-md-2">
+                                <label for="tingkat">Tingkat</label>
+                                <select class="form-control mb-3" name="tingkat" id="tingkat" required>
+                                    <option value="" selected>Pilih Tingkat</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4">
+                                <label for="kode_kk">Rombongan Belajar</label>
+                                <select class="form-control mb-3" name="kode_rombel" id="kode_rombel" required>
+                                    <option value="" selected>Pilih Rombel</option>
+                                    @foreach ($rombelOptions as $kode_rombel => $rombel)
+                                        <option value="{{ $kode_rombel }}">{{ $rombel }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="col-md-4">
+                                <label for="kode_peserta_didik">Peserta Didik</label>
+                                <select class="form-control mb-3" name="kode_peserta_didik" id="kode_peserta_didik"
+                                    required>
+                                    <option value="" selected>Pilih Peserta Didik</option>
+                                    <!-- Data akan dimuat secara dinamis -->
+                                </select>
+                            </div>
+                            <div class="col-lg-12">
+                                <div class="gap-2 hstack justify-content-end">
+                                    <button type="submit" class="btn btn-soft-success">Simpan</button>
+                                    {{-- <button type="button" class="btn btn-soft-success">Cancel</button> --}}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
