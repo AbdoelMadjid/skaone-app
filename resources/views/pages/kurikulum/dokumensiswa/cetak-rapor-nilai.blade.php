38a83f12821d1fb7cc7f27bb0c3809059ffb7fe4
abdoelmadjid <abdulmadjid.mpd@gmail.com>
2024-12-14 20:10:52 +0700

    update cetak rapor

--- resources/views/pages/kurikulum/dokumensiswa/cetak-rapor-nilai.blade.php
+++ resources/views/pages/kurikulum/dokumensiswa/cetak-rapor-nilai.blade.php
@@ -422,7 +422,7 @@
                                         <table width='100%'>
                                             <tr>
                                                 <td>
-                                                    {!! $titiMangsa->alamat ?? 'Titimangsa isi terlebih dahulu' !!}, {!! formatTanggalIndonesia($titiMangsa->titimangsa ?? '') !!} <br>
+                                                    {!! ucwords(strtolower($titiMangsa->alamat)) ?? 'Titimangsa isi terlebih dahulu' !!}, {!! formatTanggalIndonesia($titiMangsa->titimangsa ?? '') !!} <br>
                                                     Wali Kelas,
                                                     <p>&nbsp;</p>
                                                     <p>&nbsp;</p>
