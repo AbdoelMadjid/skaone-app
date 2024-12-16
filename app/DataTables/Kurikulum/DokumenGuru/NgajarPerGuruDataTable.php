ecdacf3ab1f78239d6097a5912369e9b4d244196
abdoelmadjid <abdulmadjid.mpd@gmail.com>
2024-12-16 12:10:44 +0700

    update dokumen guru

---
+++ app/DataTables/Kurikulum/DokumenGuru/NgajarPerGuruDataTable.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\DataTables\Kurikulum\DokumenGuru;
+
+use App\Models\Kurikulum\DataKBM\KbmPerRombel;
+use App\Traits\DatatableHelper;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class NgajarPerGuruDataTable extends DataTable
+{
+    use DatatableHelper;
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn('action', function ($row) {
+                // Menggunakan basicActions untuk menghasilkan action buttons
+                $actions = $this->basicActions($row);
+                return view('action', compact('actions'));
+            })
+            ->addIndexColumn();
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(KbmPerRombel $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+            ->setTableId('ngajarperguru-table')
+            ->columns($this->getColumns())
+            ->minifiedAjax()
+            //->dom('Bfrtip')
+            ->orderBy(1)
+            ->selectStyleSingle()
+            ->buttons([
+                Button::make('excel'),
+                Button::make('csv'),
+                Button::make('pdf'),
+                Button::make('print'),
+                Button::make('reset'),
+                Button::make('reload')
+            ])->parameters([
+                'lengthChange' => false, // Menghilangkan dropdown "Show entries"
+                'searching' => false,    // Menghilangkan kotak pencarian
+                'pageLength' => 50,       // Menampilkan 50 baris per halaman
+            ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('DT_RowIndex')->title('No')->orderable(false)->searchable(false)->addClass('text-center')->width(50),
+            Column::make('kode_mapel_rombel')->title('Kode Mapel Rombel'),
+            Column::make('tahunajaran')->title('Thn Ajaran')->addClass('text-center'),
+            Column::make('semester'),
+            Column::make('rombel')->title('Rombel')->addClass('text-center'),
+            Column::make('mata_pelajaran')->title('Nama Mapel'),
+            Column::make('kkm')->title('KKM')->addClass('text-center'),
+            Column::make('id_personil')->title('Guru Pengajar'),
+            /*  Column::computed('action')
+                ->exportable(false)
+                ->printable(false)
+                ->width(60)
+                ->addClass('text-center'), */
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'NgajarPerGuru_' . date('YmdHis');
+    }
+}
