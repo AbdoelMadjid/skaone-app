343ea635921e0eecc51e807e30465cef96e19fb8
abdoelmadjid <abdulmadjid.mpd@gmail.com>
2024-12-22 10:58:55 +0700

    update arsip guru

--- resources/views/auth/login-cover.blade.php
+++ resources/views/auth/login-cover.blade.php
@@ -19,9 +19,9 @@
                                         <div class="bg-overlay"></div>
                                         <div class="position-relative h-100 d-flex flex-column">
                                             <div class="mb-4">
-                                                <a href="/dashboard" class="d-block">
-                                                    <img src="{{ URL::asset('images/sakola/logo.png') }}" alt=""
-                                                        height="60">
+                                                <a href="/" class="d-block">
+                                                    <img src="{{ URL::asset('images/sakola/logo-footer.png') }}"
+                                                        alt="" height="60">
                                                 </a>
                                             </div>
                                             <div class="mt-auto">
@@ -42,16 +42,19 @@
                                                     </div>
                                                     <div class="carousel-inner text-center text-white-50 pb-5">
                                                         <div class="carousel-item active">
-                                                            <p class="fs-15 fst-italic">" Great! Clean code, clean design,
-                                                                easy for customization. Thanks very much! "</p>
+                                                            <p class="fs-15 fst-italic">" Ilmu adalah harta yang tidak akan
+                                                                pernah habis, jadi selalu tanamkan dalam diri untuk terus
+                                                                belajar "</p>
                                                         </div>
                                                         <div class="carousel-item">
-                                                            <p class="fs-15 fst-italic">" The theme is really great with an
-                                                                amazing customer support."</p>
+                                                            <p class="fs-15 fst-italic">" Kesempatan tak akan datang dua
+                                                                kali, maka perbanyak belajar untuk mendapatkan ilmu yang
+                                                                lebih luas "</p>
                                                         </div>
                                                         <div class="carousel-item">
-                                                            <p class="fs-15 fst-italic">" Great! Clean code, clean design,
-                                                                easy for customization. Thanks very much! "</p>
+                                                            <p class="fs-15 fst-italic">" Belajar adalah pintu menuju
+                                                                kesuksesan, sementara malas adalah jalan menuju kegagalan "
+                                                            </p>
                                                         </div>
                                                     </div>
                                                 </div>
@@ -179,7 +182,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <div class="text-center">
-                            <p class="mb-0 text-muted">&copy;
+                            <p class="mb-0">&copy;
                                 {{ $profileApp->app_tahun ?? '' }}
                                 <script>
                                     document.write(new Date().getFullYear())
