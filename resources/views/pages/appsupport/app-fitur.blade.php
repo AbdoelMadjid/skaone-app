ffda78d4eb53c922da0e97a0dfe4bed7d4c21fd8
AbdoelMadjid <abdulmadjid.mpd@gmail.com>
2024-12-23 15:15:44 +0700

    update PROTEKSI HAL

--- resources/views/pages/appsupport/app-fitur.blade.php
+++ resources/views/pages/appsupport/app-fitur.blade.php
@@ -10,43 +10,75 @@
             @lang('translation.app-support')
         @endslot
     @endcomponent
-    <div class="row">
-        <div class="col-lg-12">
-            <!-- Warning Alert -->
-            <div class="alert alert-warning alert-dismissible alert-additional fade show mb-2" role="alert">
-                <div class="alert-body">
-                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    <div class="d-flex">
-                        <div class="flex-shrink-0 me-3">
-                            <i class="ri-alert-line fs-16 align-middle"></i>
-                        </div>
-                        <div class="flex-grow-1">
-                            <h5 class="alert-heading">Uh oh, something went wrong!</h5>
-                            <p class="mb-0">Better check yourself, you're not looking too good. </p>
+    @if (auth()->check() &&
+            auth()->user()->hasAnyRole(['master']))
+        <div class="row">
+            <div class="col-lg-12">
+                <!-- Warning Alert -->
+                <div class="alert alert-warning alert-dismissible alert-additional fade show mb-2" role="alert">
+                    <div class="alert-body">
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        <div class="d-flex">
+                            <div class="flex-shrink-0 me-3">
+                                <i class="ri-alert-line fs-16 align-middle"></i>
+                            </div>
+                            <div class="flex-grow-1">
+                                <h5 class="alert-heading">Uh oh, something went wrong!</h5>
+                                <p class="mb-0">Better check yourself, you're not looking too good. </p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="alert-content">
-                    <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and
-                        tidy.</p>
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header d-flex align-items-center">
-                    <h5 class="card-title mb-0 flex-grow-1">@lang('translation.tables') @lang('translation.app-fitur')</h5>
-                    <div>
-                        @can('create appsupport/app-fiturs')
-                            <a class="mb-3 btn btn-primary action" href="{{ route('appsupport.app-fiturs.create') }}">Add</a>
-                        @endcan
+                    <div class="alert-content">
+                        <p class="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and
+                            tidy.</p>
                     </div>
                 </div>
-                <div class="card-body">
-                    {!! $dataTable->table(['class' => 'table table-striped hover', 'style' => 'width:100%']) !!}
+                <div class="card">
+                    <div class="card-header d-flex align-items-center">
+                        <h5 class="card-title mb-0 flex-grow-1">@lang('translation.tables') @lang('translation.app-fitur')</h5>
+                        <div>
+                            @can('create appsupport/app-fiturs')
+                                <a class="mb-3 btn btn-primary action"
+                                    href="{{ route('appsupport.app-fiturs.create') }}">Add</a>
+                            @endcan
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        {!! $dataTable->table(['class' => 'table table-striped hover', 'style' => 'width:100%']) !!}
+                    </div>
                 </div>
             </div>
+            <!--end col-->
         </div>
-        <!--end col-->
-    </div>
+    @else
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="alert alert-warning alert-dismissible alert-additional fade show mb-2" role="alert">
+                            <div class="alert-body">
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                    aria-label="Close"></button>
+                                <div class="d-flex">
+                                    <div class="flex-shrink-0 me-3">
+                                        <i class="ri-alert-line display-6 align-middle"></i>
+                                    </div>
+                                    <div class="flex-grow-1">
+                                        <h5 class="alert-heading">Mohon Maaf !!. <br>Halaman @yield('title')</h5>
+                                        <p class="mb-0">TIDAK BISA ANDA AKSES. </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="alert-content">
+                                <p class="mb-0">Scripting & Desing by. Abdul Madjid, S.Pd., M.Pd.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--end col-->
+        </div>
+    @endif
 @endsection
 @section('script')
     <script src="{{ URL::asset('build/libs/jquery/jquery.min.js') }}"></script>
--- resources/views/pages/appsupport/app-profil.blade.php
+++ resources/views/pages/appsupport/app-profil.blade.php
@@ -10,19 +10,50 @@
             @lang('translation.app-support')
         @endslot
     @endcomponent
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-header d-flex align-items-center">
-                    <h5 class="card-title mb-0 flex-grow-1">@lang('translation.tables') @lang('translation.app-profil')</h5>
-                </div>
-                <div class="card-body">
-                    {!! $dataTable->table(['class' => 'table table-striped hover', 'style' => 'width:100%']) !!}
+    @if (auth()->check() &&
+            auth()->user()->hasAnyRole(['master']))
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header d-flex align-items-center">
+                        <h5 class="card-title mb-0 flex-grow-1">@lang('translation.tables') @lang('translation.app-profil')</h5>
+                    </div>
+                    <div class="card-body">
+                        {!! $dataTable->table(['class' => 'table table-striped hover', 'style' => 'width:100%']) !!}
+                    </div>
                 </div>
             </div>
+            <!--end col-->
         </div>
-        <!--end col-->
-    </div>
+    @else
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="alert alert-warning alert-dismissible alert-additional fade show mb-2" role="alert">
+                            <div class="alert-body">
+                                <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                    aria-label="Close"></button>
+                                <div class="d-flex">
+                                    <div class="flex-shrink-0 me-3">
+                                        <i class="ri-alert-line display-6 align-middle"></i>
+                                    </div>
+                                    <div class="flex-grow-1">
+                                        <h5 class="alert-heading">Mohon Maaf !!. <br>Halaman @yield('title')</h5>
+                                        <p class="mb-0">TIDAK BISA ANDA AKSES. </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="alert-content">
+                                <p class="mb-0">Scripting & Desing by. Abdul Madjid, S.Pd., M.Pd.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--end col-->
+        </div>
+    @endif
 @endsection
 @section('script')
     <script src="{{ URL::asset('build/libs/jquery/jquery.min.js') }}"></script>
