9d66a2479cf285719c26c4b8ac1cb36d7a37c7dd
abdoelmadjid <abdulmadjid.mpd@gmail.com>
2025-05-27 21:45:51 +0700

    update perangkat ujian

--- resources/views/pages/kurikulum/perangkatujian/halamanadmin/pengawas-ujian.blade.php
+++ resources/views/pages/kurikulum/perangkatujian/halamanadmin/pengawas-ujian.blade.php
@@ -147,7 +147,53 @@
                     </tr>
                 </tbody>
             </table>
-            <br><br>
+            <table
+                style='margin: 0 auto;width:100%;border-collapse:collapse;font:12px Times New Roman;border: none !important;'>
+                <tr>
+                    <td width='25' style='border: none !important;'>&nbsp;</td>
+                    <td style='border: none !important;'>
+                        <p style='margin-bottom:-2px;margin-top:-2px'>&nbsp;</p>
+                        <table width='70%'
+                            style='margin: 0 auto;width:100%;border-collapse:collapse;font:12px Times New Roman;border: none !important;'>
+                            <tr style='border: none !important;'>
+                                <td width='50' style='border: none !important;'></td>
+                                <td style='border: none !important;'></td>
+                                <td style='border: none !important;'>
+                                    Mengetahui<br>
+                                    Kepala Sekolah,
+                                    <div>
+                                        <img src='{{ URL::asset('images/damudin.png') }}' border='0' height='110'
+                                            style=' position: absolute; padding: 0px 2px 15px -200px; margin-left: -120px;margin-top:-15px;'>
+                                    </div>
+                                    {{-- <div><img src='{{ URL::asset('images/stempel.png') }}' border='0' height='180'
+                                            width='184'
+                                            style=' position: absolute; padding: 0px 2px 15px -650px; margin-left: -135px;margin-top:-50px;'>
+                                    </div> --}}
+                                    <p>&nbsp;</p>
+                                    <p>&nbsp;</p>
+                                    <strong>H. DAMUDIN, S.Pd., M.Pd.</strong><br>
+                                    NIP. 19740302 199803 1 002
+                                </td>
+                                <td style='border: none !important;' width='200'></td>
+                                <td style='padding:4px 8px;border: none !important;'>
+                                    Majalengka, 05 Mei 2025<br>
+                                    Wakasek Kurikulum,
+                                    <div>
+                                        <img src='{{ URL::asset('images/almadjid.png') }}' border='0' height='110'
+                                            style=' position: absolute; padding: 0px 2px 15px -200px; margin-left: -20px;margin-top:-15px;'>
+                                    </div>
+
+                                    <p>&nbsp;</p>
+                                    <p>&nbsp;</p>
+                                    <strong>Abdul Madjid, S.Pd., M.Pd.</strong><br>
+                                    NIP. 19761128 200012 1 002
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td width='25' style='border: none !important;'>&nbsp;</td>
+                </tr>
+            </table>
         </div>
     </div>
     <div class="col-md-5">
@@ -180,8 +226,54 @@
                     @endforelse
                 </tbody>
             </table>
-            <br><br>
+            <table
+                style='margin: 0 auto;width:100%;border-collapse:collapse;font:12px Times New Roman;border: none !important;'>
+                <tr>
+                    <td width='25' style='border: none !important;'>&nbsp;</td>
+                    <td style='border: none !important;'>
+                        <p style='margin-bottom:-2px;margin-top:-2px'>&nbsp;</p>
+                        <table
+                            style='margin: 0 auto;width:100%;border-collapse:collapse;font:12px Times New Roman;border: none !important;'>
+                            <tr style='border: none !important;'>
+                                <td width='50' style='border: none !important;'></td>
+                                <td style='border: none !important;'></td>
+                                <td style='border: none !important;'>
+                                    Mengetahui<br>
+                                    Kepala Sekolah,
+                                    <div>
+                                        <img src='{{ URL::asset('images/damudin.png') }}' border='0' height='110'
+                                            style=' position: absolute; padding: 0px 2px 15px -200px; margin-left: -160px;margin-top:-15px;'>
+                                    </div>
+                                    {{-- <div><img src='{{ URL::asset('images/stempel.png') }}' border='0' height='180'
+                                            width='184'
+                                            style=' position: absolute; padding: 0px 2px 15px -650px; margin-left: -135px;margin-top:-50px;'>
+                                    </div> --}}
+                                    <p>&nbsp;</p>
+                                    <p>&nbsp;</p>
+                                    <strong>H. DAMUDIN, S.Pd., M.Pd.</strong><br>
+                                    NIP. 19740302 199803 1 002
+                                </td>
+                                <td style='border: none !important;' width='200'></td>
+                                <td style='padding:4px 8px;border: none !important;'>
+                                    Majalengka, 05 Mei 2025<br>
+                                    Wakasek Kurikulum,
+                                    <div>
+                                        <img src='{{ URL::asset('images/almadjid.png') }}' border='0'
+                                            height='110'
+                                            style=' position: absolute; padding: 0px 2px 15px -200px; margin-left: -80px;margin-top:-15px;'>
+                                    </div>
 
+                                    <p>&nbsp;</p>
+                                    <p>&nbsp;</p>
+                                    <strong>Abdul Madjid, S.Pd., M.Pd.</strong><br>
+                                    NIP. 19761128 200012 1 002
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                    <td width='25' style='border: none !important;'>&nbsp;</td>
+                </tr>
+            </table>
         </div>
     </div>
 </div>
