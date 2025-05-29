9405e7d8fa888762d82de84224108d7544d42948
abdoelmadjid <abdulmadjid.mpd@gmail.com>
2025-05-29 13:49:12 +0700

    update perangkat ujian

--- resources/views/pages/kurikulum/perangkatujian/halamanadmin/jadwal-ujian-tampil.blade.php
+++ resources/views/pages/kurikulum/perangkatujian/halamanadmin/jadwal-ujian-tampil.blade.php
@@ -1,13 +1,16 @@
-<h4 class="text-center">JADWAL UJIAN TINGKAT {{ $tingkat }}</h4>
-<h4 class="text-center">{{ strtoupper($identitasUjian?->nama_ujian ?? '-') }}</h4>
-<h4 class="text-center">TAHUN AJARAN {{ $identitasUjian?->tahun_ajaran ?? '-' }}</h4>
-<table class="table table-bordered">
+<img class="card-img-top img-fluid mb-0" src="{{ URL::asset('images/kossurat.jpg') }}" alt="Card image cap"><br><br>
+<div style="text-align:center; font-size: 14px; font-weight: bold;">
+    <h4 class="text-center">JADWAL UJIAN TINGKAT {{ $tingkat }}</h4>
+    <h4 class="text-center">{{ strtoupper($identitasUjian?->nama_ujian ?? '-') }}</h4>
+    <h4 class="text-center">TAHUN AJARAN {{ $identitasUjian?->tahun_ajaran ?? '-' }}</h4>
+</div>
+<table class="table table-bordered" style="font-size: 12px;" width="100%">
     <thead>
         <tr>
-            <th rowspan="2">NO</th>
-            <th rowspan="2">HARI / TANGGAL</th>
-            <th rowspan="2">JAM KE-</th>
-            <th rowspan="2">PUKUL</th>
+            <th rowspan="2" class="text-center align-middle">NO</th>
+            <th rowspan="2" class="text-center align-middle">HARI / TANGGAL</th>
+            <th rowspan="2" class="text-center align-middle">JAM KE-</th>
+            <th rowspan="2" class="text-center align-middle">PUKUL</th>
             <th colspan="{{ count($kodeKKList) }}">KELAS {{ $tingkat }}</th>
         </tr>
         <tr>
@@ -42,4 +45,50 @@
             @endforeach
         @endforeach
     </tbody>
+</table>
+<table style='margin: 0 auto;width:100%;border-collapse:collapse;font:12px Times New Roman;border: none !important;'>
+    <tr>
+        <td width='25' style='border: none !important;'>&nbsp;</td>
+        <td style='border: none !important;'>
+            <p style='margin-bottom:-2px;margin-top:-2px'>&nbsp;</p>
+            <table
+                style='margin: 0 auto;width:100%;border-collapse:collapse;font:12px Times New Roman;border: none !important;'>
+                <tr style='border: none !important;'>
+                    <td width='50' style='border: none !important;'></td>
+                    <td style='border: none !important;'></td>
+                    <td style='border: none !important;'>
+                        Mengetahui<br>
+                        Kepala Sekolah,
+                        <div>
+                            <img src='{{ URL::asset('images/damudin.png') }}' border='0' height='110'
+                                style=' position: absolute; padding: 0px 2px 15px -200px; margin-left: -160px;margin-top:-15px;'>
+                        </div>
+                        {{-- <div><img src='{{ URL::asset('images/stempel.png') }}' border='0' height='180'
+                                            width='184'
+                                            style=' position: absolute; padding: 0px 2px 15px -650px; margin-left: -135px;margin-top:-50px;'>
+                                    </div> --}}
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                        <strong>H. DAMUDIN, S.Pd., M.Pd.</strong><br>
+                        NIP. 19740302 199803 1 002
+                    </td>
+                    <td style='border: none !important;' width='200'></td>
+                    <td style='padding:4px 8px;border: none !important;'>
+                        Majalengka, 05 Mei 2025<br>
+                        Ketua Panitia,
+                        {{-- <div>
+                                <img src='{{ URL::asset('images/almadjid.png') }}' border='0' height='110'
+                                    style=' position: absolute; padding: 0px 2px 15px -200px; margin-left: -80px;margin-top:-15px;'>
+                            </div> --}}
+
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                        <strong>ABDUL MADJID, S.Pd., M.Pd.</strong><br>
+                        NIP. 19761128 200012 1 002
+                    </td>
+                </tr>
+            </table>
+        </td>
+        <td width='25' style='border: none !important;'>&nbsp;</td>
+    </tr>
 </table>
--- resources/views/pages/kurikulum/perangkatujian/halamanadmin/jadwal-ujian.blade.php
+++ resources/views/pages/kurikulum/perangkatujian/halamanadmin/jadwal-ujian.blade.php
@@ -20,10 +20,17 @@
                                 </select>
                             </div>
                         </div>
-                        <div class="col-lg-auto">
+                        {{-- <div class="col-lg-auto">
                             <div class="mb-3 d-flex align-items-center gap-2">
                                 <button type="button" class="btn btn-soft-primary" id="btn-download-jadwal">
-                                    Download Jadwal
+                                    Download
+                                </button>
+                            </div>
+                        </div> --}}
+                        <div class="col-lg-auto">
+                            <div class="mb-3 d-flex align-items-center gap-2">
+                                <button type="button" class="btn btn-soft-primary" id="btn-cetak-jadwal">
+                                    Cetak
                                 </button>
                             </div>
                         </div>
@@ -38,3 +45,64 @@
 <div id="tabel-jadwal-ujian" class="mb-3">
 
 </div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const printButton = document.getElementById('btn-cetak-jadwal');
+        if (!printButton) {
+            console.error("Tombol print tidak ditemukan");
+            return;
+        }
+
+        printButton.addEventListener('click', function() {
+            /* const content = document.getElementById('tabel-jadwal-ujian');
+            if (!content) {
+                console.error("Elemen tabel tidak ditemukan");
+                return;
+            } */
+
+            const content = document.getElementById('tabel-jadwal-ujian');
+            //const tingkat = document.getElementById('tingkat').value || 'jadwal';
+
+            if (!content || !content.innerHTML.trim()) {
+                showToast('error',
+                    'Silakan pilih tingkat terlebih dahulu dan pastikan jadwal sudah ditampilkan.');
+                return;
+            }
+
+            const win = window.open('', '_blank');
+            win.document.write(`
+            <html>
+            <head>
+                <title>Daftar Pengawas</title>
+                <style>
+                    @page {
+                        size: A4;
+                        margin: 5mm;
+                    }
+                    html, body {
+                        width: 210mm;
+                        height: 297mm;
+                        margin: 0;
+                        padding: 0;
+                        font-family: 'Times New Roman', serif;
+                        font-size: 12px;
+                    }
+                    table { width: 100%; border-collapse: collapse;}
+                    table, th, td { border: 1px solid black; }
+                    th, td { padding: 5px; text-align: center; }
+                    h4 { margin: 5px 0; text-align: center; }
+                </style>
+            </head>
+            <body>
+                ${content.innerHTML}
+            </body>
+            </html>
+        `);
+            win.document.close();
+            win.focus();
+            win.print();
+            win.close();
+        });
+    });
+</script>
