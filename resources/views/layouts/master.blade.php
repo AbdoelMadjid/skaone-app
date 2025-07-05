4b32672fd7f3d32d4a772a52ef03b0895e0a9173
AbdoelMadjid <abdulmadjid.mpd@gmail.com>
2025-07-05 19:59:49 +0700

    update fitur aplikasi theme costum

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,68 +2,73 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout="vertical" data-topbar="light" data-sidebar="dark"
     data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
 
-<head>
-    <meta charset="utf-8" />
-    <meta name="csrf_token" content="{{ csrf_token() }}">
-    <title>@yield('title') | {{ $profileApp->app_nama ?? '' }}</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="{{ $profileApp->app_deskripsi ?? '' }}" name="description" />
-    <meta content="{{ $profileApp->app_pengembang ?? '' }}" name="author" />
-    <!-- App favicon -->
-    <link rel="shortcut icon"
-        href="{{ $profileApp->app_icon ? URL::asset('build/images/' . $profileApp->app_icon) : URL::asset('build/images/icon-blue.png') }}">
-    @include('layouts.head-css')
-</head>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="csrf_token" content="{{ csrf_token() }}">
+        <title>@yield('title') | {{ $profileApp->app_nama ?? '' }}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="{{ $profileApp->app_deskripsi ?? '' }}" name="description" />
+        <meta content="{{ $profileApp->app_pengembang ?? '' }}" name="author" />
+        <!-- App favicon -->
+        <link rel="shortcut icon"
+            href="{{ $profileApp->app_icon ? URL::asset('build/images/' . $profileApp->app_icon) : URL::asset('build/images/icon-blue.png') }}">
+        @include('layouts.head-css')
+    </head>
 
-@section('body')
-    @include('layouts.body')
-@show
-<!-- Begin page -->
-<div id="layout-wrapper">
-    @include('layouts.topbar')
-    @include('layouts.sidebar')
-    <!-- ============================================================== -->
-    <!-- Start right Content here -->
-    <!-- ============================================================== -->
-    <div class="main-content">
-        <div class="page-content">
-            <div class="container-fluid">
-                <div class="modal fade" id="modal_action" tabindex="-1"
-                    aria-labelledby="myModalLabel"aria-hidden="true">
+    @section('body')
+        @include('layouts.body')
+    @show
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+        @include('layouts.topbar')
+        @include('layouts.sidebar')
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+            <div class="page-content">
+                <div class="container-fluid">
+                    <div class="modal fade" id="modal_action" tabindex="-1"
+                        aria-labelledby="myModalLabel"aria-hidden="true">
+                    </div>
+                    @yield('content')
                 </div>
-                @yield('content')
+                <!-- container-fluid -->
             </div>
-            <!-- container-fluid -->
-        </div>
 
-        <!-- End Page-content -->
-        @include('layouts.footer')
+            <!-- End Page-content -->
+            @include('layouts.footer')
 
-        <div class="preloader" style="visibility:hidden;">
-            <div class="lds-ellipsis">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div class="preloader" style="visibility:hidden;">
+                <div class="lds-ellipsis">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
             </div>
+
         </div>
+        <!-- end main content-->
 
     </div>
-    <!-- end main content-->
+    <!-- END layout-wrapper -->
 
-</div>
-<!-- END layout-wrapper -->
+    @php
+        $iscustomizerActive = App\Helpers\Fitures::isFiturAktif('theme-customizer');
+    @endphp
 
-<button onclick="topFunction()" class="btn btn-danger btn-icon" id="back-to-top">
-    <i class="ri-arrow-up-line"></i>
-</button>
-
-@if (auth()->check() &&
-        auth()->user()->hasAnyRole(['master']))
-    @include('layouts.customizer')
-@endif
-<!-- JAVASCRIPT -->
-@include('layouts.vendor-scripts')
-</body>
+    @if ($iscustomizerActive)
+        @if (auth()->check() &&
+                auth()->user()->hasAnyRole(['master']))
+            @include('layouts.customizer')
+        @endif
+    @endif
+    <button onclick="topFunction()" class="btn btn-danger btn-icon" id="back-to-top">
+        <i class="ri-arrow-up-line"></i>
+    </button>
+    <!-- JAVASCRIPT -->
+    @include('layouts.vendor-scripts')
+    </body>
 
 </html>
